516cf9db3a5af3b87e0baea49b595eee7459c05b
kai <eigenspaces@gmail.com>
2023-03-29 21:24:01 -0500

    implementing react markdown

--- src/App.js
+++ src/App.js
@@ -1,41 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-import { marked } from 'marked';
+// import { marked } from 'marked';
+import { Remarkable } from 'remarkable';
 import { sampleText } from './sampleText.js';
 import { render } from '@testing-library/react';
+import { useState } from 'react';
 
-function Render() {
+const md = new Remarkable();
+
+function handleChange(markdown) {
+  const inHTML = md.render(markdown);
+  return {__html: inHTML};
+}
+function RenderText({ markdown }) {
+  const outMarkup = handleChange(markdown)
+  return <div dangerouslySetInnerHTML={outMarkup}/>
+}
+
+export default function Editor() {
+  const mkdStr = `
+# Markdown Editor
+
+---
+
+**Hello world!!!**
+
+[![](https://avatars.githubusercontent.com/u/1680273?s=80&v=4)](https://avatars.githubusercontent.com/u/1680273?v=4)
+
+\`\`\`javascript
+import React from "react";
+import ReactDOM from "react-dom";
+import MEDitor from '@uiw/react-md-editor';
+
+\`\`\`
+`;
+const [value, setValue] = useState();
+
   return (
     <div className='container'>
       <div className='row'>
         <div className='col-sm-6'>
-          <textarea className='form-control'
-            onChange={this.handleChange}
-            value={this.state.text}
-            className='form-control'
-            rows='35'
-          />
-        </div>
+        <textarea className='form-control'
+          onChange={setValue}
+          value={value}
+          rows='35'
+        />
+      </div>
         <div className='col-sm-6'>
-          <div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
+           <RenderText />
         </div>
       </div>
     </div>
   );
-}
-export default render;
-
-class App {
-  state = {
-    text: sampleText
-  }
-
-  handleChange = event => {
-    const text = event.target.value
-    this.setState({ text })
-  }
-  renderText = text => {
-    const __html = marked(text, {sanitize: true})
-    return { __html }
-  }
 }
